031600a911d54a9eb45c9a27d0f9ecfdec331dca
mehmetkurnaz <114507830+mehmetkurnaz@users.noreply.github.com>
2022-09-28 17:46:56 +0300

    Add files via upload

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,12 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortıslem = new DortIslem();
+		System.out.println(dortıslem.topla(2,3));
+		dortıslem.topla1(2,3,5);
+System.out.println(dortıslem.topla1(2,3,5));
+	}
+
+}
